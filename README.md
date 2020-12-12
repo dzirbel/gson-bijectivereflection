ac544d9691df154d83ad4c899e143cf57104298c
Dominic Zirbel <dominiczirbel@gmail.com>
2020-12-11 21:01:02 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# GSON Bijective Reflection
+
+`gson-bijectivereflection` is an extension to GSON which adds stricter deserialization of
+user-provided classes.
+
+names:
+- github repo name: gson-bijectivereflection
+- gradle project name: gson-bijectivereflection
+- maven package/name: groupId: com.dominiczirbel, artifactId: gson-bijectivereflection
+- code package: com.dominiczirbel.gson.bijectivereflection.*
---
+++ build.gradle.kts
@@ -0,0 +1,60 @@
+import org.gradle.api.tasks.testing.logging.TestLogEvent
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+private object Versions {
+    const val detekt = "1.15.0-RC1" // https://github.com/detekt/detekt; also update plugin version
+    const val gson = "2.8.6" // https://github.com/google/gson
+    const val junit = "5.7.0" // https://junit.org/junit5/
+    const val kotlinReflect = "1.4.21" // https://kotlinlang.org/docs/reference/reflection.html
+    const val truth = "1.1" // https://truth.dev/
+}
+
+plugins {
+    // https://kotlinlang.org/releases.html
+    kotlin("jvm") version "1.4.21"
+
+    // https://github.com/detekt/detekt; also update dependency version
+    id("io.gitlab.arturbosch.detekt") version "1.15.0-RC1"
+}
+
+version = "0.1"
+
+repositories {
+    mavenCentral()
+    jcenter()
+}
+
+dependencies {
+    implementation("com.google.code.gson", "gson", Versions.gson)
+    implementation("org.jetbrains.kotlin", "kotlin-reflect", Versions.kotlinReflect)
+
+    testImplementation("org.junit.jupiter", "junit-jupiter-api", Versions.junit)
+    testImplementation("org.junit.jupiter", "junit-jupiter-params", Versions.junit)
+    testRuntimeOnly("org.junit.jupiter", "junit-jupiter-engine", Versions.junit)
+    testImplementation("com.google.truth", "truth", Versions.truth)
+
+    detektPlugins("io.gitlab.arturbosch.detekt", "detekt-formatting", Versions.detekt)
+}
+
+tasks.withType<KotlinCompile>().configureEach {
+    kotlinOptions.allWarningsAsErrors = true
+    kotlinOptions.jvmTarget = "1.8"
+}
+
+configurations.all {
+    resolutionStrategy {
+        failOnNonReproducibleResolution()
+    }
+}
+
+tasks.test {
+    useJUnitPlatform()
+    testLogging {
+        events = setOf(TestLogEvent.FAILED, TestLogEvent.STANDARD_ERROR, TestLogEvent.STANDARD_OUT)
+    }
+}
+
+detekt {
+    input = files("src/main/kotlin", "src/test/kotlin")
+    config = files("detekt-config.yml")
+}
